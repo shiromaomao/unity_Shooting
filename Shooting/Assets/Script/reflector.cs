49b5f6cd4a28f14d2ebe562283e449228661d83c
shiromaomao <securewanko+yuito@gmail.com>
2021-09-24 19:08:31 +0900

    Limit<refrector>
    playerが動かすrefrectorの移動範囲の制限に成功。それに伴い移動スピードの変更
    
    原因：移動のところにMathf.Clampを入れてなかった。

--- Shooting/Assets/Script/reflector.cs
+++ Shooting/Assets/Script/reflector.cs
@@ -9,38 +9,45 @@ public class reflector : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
-    //ˆÚ“®(x 12‚©‚ç-12  y 1.275‚©‚ç8.75)
-    /*void Clamp()
+
+
+    // Update is called once per frame
+    void Update()
     {
         player_position.x = transform.position.x;
         player_position.y = transform.position.y;
 
-        player_position.x = Mathf.Clamp(player_position.x, 12, -12);
-        player_position.y = Mathf.Clamp(player_position.y, 1.275f, -8.75f);
-    }*/
-    // Update is called once per frame
-    void Update()
-    {
+        //ˆÚ“®(x 12‚©‚ç-12  y 1.275‚©‚ç8.75)
+        //player_position.x = Mathf.Clamp(player_position.x, -12, 12);
+        //player_position.y = Mathf.Clamp(player_position.y, -8.75f, 1.275f);
+
         if (Input.GetKey("w"))
         {
-            transform.position += transform.up * 0.025f;
-        }
+            //transform.position += transform.up * 0.025f;
+            player_position.y = Mathf.Clamp(transform.position.y + 0.075f, 1.275f, 8.75f);
 
+        }
         if (Input.GetKey("s"))
         {
-            transform.position -= transform.up * 0.025f;
+            //transform.position -= transform.up * 0.025f;
+            player_position.y = Mathf.Clamp(transform.position.y - 0.075f, 1.275f, 8.75f);
         }
 
         if (Input.GetKey("a"))
         {
-            transform.position -= transform.right * 0.05f;
+            //transform.position -= transform.right * 0.05f;
+            player_position.x = Mathf.Clamp(transform.position.x - 0.1f, -12, 12);
         }
 
         if (Input.GetKey("d"))
         {
-            transform.position += transform.right * 0.05f;
+            //transform.position += transform.right * 0.05f;
+            player_position.x = Mathf.Clamp(transform.position.x + 0.1f, -12, 12);
         }
+
+        transform.position = new Vector3(player_position.x, player_position.y, -22);
+
     }
 }
---
+++ Shooting/Assets/Script/warpsystem.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class warpsystem : MonoBehaviour
+{
+    int count= 0;
+    private void OnCollisionEnter(Collision col)
+    {
+        Debug.Log(col.gameObject.name); // ‚Ô‚Â‚©‚Á‚½‘ŠŽè‚Ì–¼‘O‚ðŽæ“¾
+        if (col.gameObject.tag == "Block")
+        {
+            if (count == 10)
+            {
+                transform.position = new Vector3(17, 34, -3);
+                count = 0;
+            }
+            count += 1;
+        }
+    }
+                                        // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
